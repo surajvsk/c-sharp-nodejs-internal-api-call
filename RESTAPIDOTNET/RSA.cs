fcc90c88e8bd89ceb33bc817ecb9ce929fb51b73
Suraj Vishwakarma <surajvishwakarma@protonmail.com>
2022-11-20 20:16:56 +0530

    working on rsa

---
+++ RESTAPIDOTNET/RSA.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTAPIDOTNET
+{
+    internal class RSA
+    {
+
+        public string Encryption(string strText)
+        {
+            var publicKey = @"-----BEGIN RSA PUBLIC KEY-----
+MIIBCgKCAQEAyGVfRqJ1KGkLh/cG6qiKKexZSTEcVEk3t3UkYdkfgWFd4VylNd36
+V7ckE+qCNhM8piXoVZX4WZFFs8S4ByzWPn3wK5lSpZDwDlqFb0fG2mnNyGGpM5Eq
+u0rB6XqiPkS2Bre5gSSxu5agyVog5wyPYAwklLpi7EFWpsRi3atEhqzWS+deopNk
+mmu5+dmhzOTs+eO/LctrqShqoIMLWCY0k4NMczhHXxuc8PMM/UnbTbm3hwQ5VDjD
+MHz3qJksBpGRd8RzLz2D/YOL5ZQAj/IP9iZjf1WV4b3Gr6ZTLNhB5kfX6xEFZm9v
+XHCDhKY/A1oWiEIc6AthlDMhoyqpszWGIwIDAQAB
+-----END RSA PUBLIC KEY-----";
+
+
+            var testData = Encoding.UTF8.GetBytes(strText);
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                try
+                {
+                    // client encrypting data with public key issued by server                    
+                    // rsa.FromXmlString(publicKey);
+
+                    rsa.FromXmlString(publicKey);
+                    var encryptedData = rsa.Encrypt(testData, true);
+                    var base64Encrypted = Convert.ToBase64String(encryptedData);
+                    return base64Encrypted;
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+        }
+
+        public static string Decryption(string strText)
+        {
+            var privateKey = "<RSAKeyValue><Modulus>21wEnTU+mcD2w0Lfo1Gv4rtcSWsQJQTNa6gio05AOkV/Er9w3Y13Ddo5wGtjJ19402S71HUeN0vbKILLJdRSES5MHSdJPSVrOqdrll/vLXxDxWs/U0UT1c8u6k/Ogx9hTtZxYwoeYqdhDblof3E75d9n2F0Zvf6iTb4cI7j6fMs=</Modulus><Exponent>AQAB</Exponent><P>/aULPE6jd5IkwtWXmReyMUhmI/nfwfkQSyl7tsg2PKdpcxk4mpPZUdEQhHQLvE84w2DhTyYkPHCtq/mMKE3MHw==</P><Q>3WV46X9Arg2l9cxb67KVlNVXyCqc/w+LWt/tbhLJvV2xCF/0rWKPsBJ9MC6cquaqNPxWWEav8RAVbmmGrJt51Q==</Q><DP>8TuZFgBMpBoQcGUoS2goB4st6aVq1FcG0hVgHhUI0GMAfYFNPmbDV3cY2IBt8Oj/uYJYhyhlaj5YTqmGTYbATQ==</DP><DQ>FIoVbZQgrAUYIHWVEYi/187zFd7eMct/Yi7kGBImJStMATrluDAspGkStCWe4zwDDmdam1XzfKnBUzz3AYxrAQ==</DQ><InverseQ>QPU3Tmt8nznSgYZ+5jUo9E0SfjiTu435ihANiHqqjasaUNvOHKumqzuBZ8NRtkUhS6dsOEb8A2ODvy7KswUxyA==</InverseQ><D>cgoRoAUpSVfHMdYXW9nA3dfX75dIamZnwPtFHq80ttagbIe4ToYYCcyUz5NElhiNQSESgS5uCgNWqWXt5PnPu4XmCXx6utco1UVH8HGLahzbAnSy6Cj3iUIQ7Gj+9gQ7PkC434HTtHazmxVgIR5l56ZjoQ8yGNCPZnsdYEmhJWk=</D></RSAKeyValue>";
+            var testData = Encoding.UTF8.GetBytes(strText);
+            using (var rsa = new RSACryptoServiceProvider(1024))
+            {
+                try
+                {
+                    var base64Encrypted = strText;
+                    // server decrypting data with private key                    
+                    rsa.FromXmlString(privateKey);
+                    var resultBytes = Convert.FromBase64String(base64Encrypted);
+                    var decryptedBytes = rsa.Decrypt(resultBytes, true);
+                    var decryptedData = Encoding.UTF8.GetString(decryptedBytes);
+                    return decryptedData.ToString();
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+        }
+    }
+}
